beefe2704d4171fab48a000bb059cfc528c9efc7
gradel1 <96313800+gradel1@users.noreply.github.com>
2023-03-25 13:44:40 +0300

    Update student.rb

--- 2lab/1 (переделанная)/student.rb
+++ 2lab/1 (переделанная)/student.rb
@@ -1,19 +1,20 @@
 class Student
-	attr_accessor :ID, :Last_name, :First_name, :Patronymic, :Phone, :Telegram, :Email, :Git
+	attr_accessor :id, :last_name, :first_name, :patronymic, :phone, :telegram, :email, :git
 	
 	def initialize(last_name:,first_name:,patronymic:, id: nil, phone: nil, telegram: nil, email: nil, git: nil)
-		self.Last_name= last_name
-		self.First_name= first_name
-		self.Patronymic= patronymic
-		self.ID= id
-		self.Phone= phone
-		self.Telegram= telegram
-		self.Email= email
-		self.Git= git
+		self.last_name= last_name
+		self.first_name= first_name
+		self.patronymic= patronymic
+		self.id= id
+		self.phone= phone
+		self.telegram= telegram
+		self.email= email
+		self.git= git
 	end
 	
 	def to_s
-		"ID: #{@ID}, Фамилия: #{@Last_name}, Имя: #{@First_name}, Отчество: #{@Patronymic}, Телефон: #{@Phone}, Телеграм: #{@Telegram}, Почта: #{@Email}, Гит: #{@Git}"
+		"ID: #{@id}, Фамилия: #{@last_name}, Имя: #{@first_name}, Отчество: #{@patronymic}, Телефон: #{@phone}, Телеграм: #{@telegram}, Почта: #{@email}, Гит: #{@git}"
 	end
 
 end
+
