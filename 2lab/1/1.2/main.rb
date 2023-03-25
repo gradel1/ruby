bd9ca436c9bd72e2c80c8a2ae181b43b2d58a38f
gradel1 <96313800+gradel1@users.noreply.github.com>
2023-03-25 09:16:51 +0300

    Create main.rb

---
+++ 2lab/1/1.2/main.rb
@@ -0,0 +1,9 @@
+load("student.rb")
+
+student1 = Student.new("Градель", "Александр", "Олегович")
+student2 = Student.new("Григорьян","Артуш","Артемович")
+student3 = Student.new("Сорока","Никита","Андреевич")
+
+puts student1.status
+puts student2.status
+puts student3.status
