624dcfa222cc1536450182468000a318ae718ee4
gradel1 <96313800+gradel1@users.noreply.github.com>
2023-05-06 13:00:39 +0300

    Update Student_short.rb

--- 2lab/1 (переделанная)/Student_short.rb
+++ 2lab/1 (переделанная)/Student_short.rb
@@ -14,10 +14,19 @@ class Student_short
 		Student_short.new(id: student.id, fullname: "#{student.last_name} #{student.first_name[0]}. #{student.patronymic[0]}.", git: student.git, contact: student.first_contact)
 	end
 
+	def self.any_contact(hash)
+		if hash["phone"] != nil then return hash["phone"] end
+    		if hash["email"] != nil then return hash["email"] end
+    		if hash["telegram"] != nil then return hash["telegram"] end
+    		return nil
+	end
+
 	def self.from_s(s,id)
 		hash = s.split(", ").map {|i| i.split(": ")}.to_h
-		Student_short.new(fullname: hash["fullname"], id: id, git: hash["git"], contact: hash["phone"])
+		Student_short.new(fullname: hash["fullname"], id: id, git: hash["git"], contact: self.any_contact(hash))
 	end
 
 end
 
+student = Student_short.from_s("fullname: Градель А.О., git: gradel1, phone: 79183333333",10)
+puts student.contact
