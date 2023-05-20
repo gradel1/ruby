15c2f016961eda3a6c9fca035d31583581b9b853
gradel1 <96313800+gradel1@users.noreply.github.com>
2023-05-20 13:25:10 +0300

    Update data_list_student_short.rb

--- 2lab/3/data_list_student_short.rb
+++ 2lab/3/data_list_student_short.rb
@@ -9,15 +9,10 @@ class Data_list_student_short < Data_list
     @@number+=1
     str=data[0]
     hash = str.split(', ').map{|el| el.split(': ')}.to_h
-    attributes = []
-    get_names.each {|el| attributes << hash[el]}
-    Data_table.new([@@number]+attributes)
+    Data_table.new([@@number]+[hash["Initials"]]+[hash["Contact"]]+ [hash["Github link"]])
   end
 
   def get_names
-    str=data[0]
-    hash = str.split(', ').map{|el| el.split(': ')}.to_h
-    hash.keys[1...hash.keys.length]
+    ["ФИО","Контакт","Гит"]
   end
-
 end
