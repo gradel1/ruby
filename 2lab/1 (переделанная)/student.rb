84934278cea595f74831b0b36d79c91b4c784abe
gradel1 <96313800+gradel1@users.noreply.github.com>
2023-05-27 09:56:24 +0300

    Update student.rb

--- 2lab/1 (переделанная)/student.rb
+++ 2lab/1 (переделанная)/student.rb
@@ -1,113 +1,118 @@
-load('student_abstract.rb')
+#load('student_abstract.rb')
+require_relative 'student_abstract'
 
 class Student < Student_abstract
-	attr_reader :phone, :last_name, :first_name, :patronymic, :telegram, :email
-	
-	def initialize(last_name:,first_name:,patronymic:, id: nil, phone: nil, telegram: nil, email: nil, git: nil)
-		self.last_name= last_name
-		self.first_name= first_name
-		self.patronymic= patronymic
-		self.id= id
-		self.phone= phone
-		self.telegram= telegram
-		self.email= email
-		self.git= git
-	end
+  attr_reader :phone, :last_name, :first_name, :patronymic, :telegram, :email
 
-	def self.from_s(s)
-		hash = s.split(", ").map {|i| i.split(": ")}.to_h
-		Student.new(last_name: hash["last_name"], first_name: hash["first_name"], patronymic: hash["patronymic"], id: hash["id"], phone: hash["phone"], telegram: hash["telegram"], email: hash["email"], git: hash["git"])
-	end
-	
-	def to_s
-		"ID: #{@id}, Фамилия: #{@last_name}, Имя: #{@first_name}, Отчество: #{@patronymic}, Телефон: #{@phone}, Телеграм: #{@telegram}, Почта: #{@email}, Гит: #{@git}"
-	end
+  def initialize(last_name:,first_name:,patronymic:, id: nil, phone: nil, telegram: nil, email: nil, git: nil)
+    self.last_name= last_name
+    self.first_name= first_name
+    self.patronymic= patronymic
+    self.id= id
+    self.phone= phone
+    self.telegram= telegram
+    self.email= email
+    self.git= git
+  end
 
-	def self.phone_valid?(phone)
-		phone.match /\A\+?\d{11}\z/
-	end
+  def self.from_s(s)
+    hash = s.split(", ").map {|i| i.split(": ")}.to_h
+    Student.new(last_name: hash["last_name"], first_name: hash["first_name"], patronymic: hash["patronymic"], id: hash["id"], phone: hash["phone"], telegram: hash["telegram"], email: hash["email"], git: hash["git"])
+  end
 
-	def phone= (phone)
-		if !phone.nil? and Student.phone_valid?(phone)==nil then raise ArgumentError,"Invalid phone number" end
-		@phone = phone
-	end
+  #def to_s
+  #"ID: #{@id}, Фамилия: #{@last_name}, Имя: #{@first_name}, Отчество: #{@patronymic}, Телефон: #{@phone}, Телеграм: #{@telegram}, Почта: #{@email}, Гит: #{@git}"
+  #end
 
-	def self.id_valid?(id)
-		id.to_s.match /^[0-9]+$/
-	end
+  def to_s
+    "id: #{@id}, last_name: #{@last_name}, first_name: #{@first_name}, patronymic: #{@patronymic}, phone: #{@phone}, telegram: #{@telegram}, email: #{@email}, git: #{@git}"
+  end
 
-	def id= (id)
-		if !id.nil? and Student.id_valid?(id)==nil then raise ArgumentError,"Invalid ID" end
-		@id=id
-	end
+  def self.phone_valid?(phone)
+    phone.match /\A\+?\d{11}\z/
+  end
 
-	def self.name_valid?(name)
-		name.match /^[A-Z][^A-Z\d]+$/
-	end
+  def phone= (phone)
+    if !phone.nil? and Student.phone_valid?(phone)==nil then raise ArgumentError,"Invalid phone number" end
+    @phone = phone
+  end
 
-	def last_name= (last_name)
-		if !last_name.nil? and Student.name_valid?(last_name)==nil then raise ArgumentError,"Invalid last name" end
-		@last_name=last_name
-	end
+  def self.id_valid?(id)
+    id.to_s.match /^[0-9]+$/
+  end
 
-	def first_name= (first_name)
-		if !first_name.nil? and Student.name_valid?(first_name)==nil then raise ArgumentError,"Invalid first name" end
-		@first_name=first_name
-	end
+  def id= (id)
+    if !id.nil? and Student.id_valid?(id)==nil then raise ArgumentError,"Invalid ID" end
+    @id=id
+  end
 
-	def patronymic= (patronymic)
-		if !patronymic.nil? and Student.name_valid?(patronymic)==nil then raise ArgumentError,"Invalid patronymic" end
-		@patronymic=patronymic
-	end
+  def self.name_valid?(name)
+    name.match /^[A-Z][^A-Z\d]+$/
+  end
 
-	def self.telegram_valid?(telegram)
-		telegram.match /^[\w\d\s]+$/
-	end
+  def last_name= (last_name)
+    if !last_name.nil? and Student.name_valid?(last_name)==nil then raise ArgumentError,"Invalid last name" end
+    @last_name=last_name
+  end
 
-	def telegram= (telegram)
-		if !telegram.nil? and Student.telegram_valid?(telegram)==nil then raise ArgumentError,"Invalid telegram" end
-		@telegram=telegram
-	end
+  def first_name= (first_name)
+    if !first_name.nil? and Student.name_valid?(first_name)==nil then raise ArgumentError,"Invalid first name" end
+    @first_name=first_name
+  end
 
-	def self.email_valid?(email)
-		email.match /^[\d\w]+\@gmail.com$/
-	end
+  def patronymic= (patronymic)
+    if !patronymic.nil? and Student.name_valid?(patronymic)==nil then raise ArgumentError,"Invalid patronymic" end
+    @patronymic=patronymic
+  end
 
-	def email= (email)
-		if !email.nil? and Student.email_valid?(email)==nil then raise ArgumentError,"Invalid email" end
-		@email=email
-	end
+  def self.telegram_valid?(telegram)
+    telegram.match /^[\w\d\s]+$/
+  end
 
-	def self.git_valid?(git)
-		git.match /^[\d\w]+$/
-	end
+  def telegram= (telegram)
+    if !telegram.nil? and Student.telegram_valid?(telegram)==nil then raise ArgumentError,"Invalid telegram" end
+    @telegram=telegram
+  end
 
-	def git= (git)
-		if !git.nil? and Student.git_valid?(git)==nil then raise ArgumentError,"Invalid git" end
-		@git=git
-	end
+  def self.email_valid?(email)
+    email.match /^[\d\w]+\@gmail.com$/
+  end
 
-	def git_valid?
-		self.git != nil
-	end
+  def email= (email)
+    if !email.nil? and Student.email_valid?(email)==nil then raise ArgumentError,"Invalid email" end
+    @email=email
+  end
 
-	def contacts_valid?
-		self.phone != nil or self.email != nil or self.telegram != nil
-	end
+  def self.git_valid?(git)
+    git.match /^[\d\w]+$/
+  end
 
-	def validate
-		git_valid? and contacts_valid?
-	end
+  def git= (git)
+    if !git.nil? and Student.git_valid?(git)==nil then raise ArgumentError,"Invalid git" end
+    @git=git
+  end
 
-    def set_contacts(phone: nil, telegram: nil, git: nil)
-		self.phone= phone
-		self.telegram= telegram
-		self.email= email
-    end
+  def git_valid?
+    self.git != nil
+  end
 
-    def getInfo
-    	"fullname: #{self.last_name} #{self.first_name[0]}. #{self.patronymic[0]}., #{if git_valid? then "git: #{self.git}, " end}#{Student_abstract.any_contact}"
-    end
+  def contacts_valid?
+    self.phone != nil or self.email != nil or self.telegram != nil
+  end
+
+  def validate
+    git_valid? and contacts_valid?
+  end
+
+  def set_contacts(phone: nil, telegram: nil, git: nil)
+    self.phone= phone
+    self.telegram= telegram
+    self.email= email
+  end
+
+  def getInfo
+    "fullname: #{self.last_name} #{self.first_name[0]}. #{self.patronymic[0]}., #{if git_valid? then "git: #{self.git}, " end}#{Student_abstract.any_contact(phone: self.phone, email: self.email, telegram: self.telegram)}"
+  end
 
 end
 
@@ -119,3 +124,7 @@ student1 = Student.new(last_name: "Gradel", first_name: "Alexander", patronymic:
 #puts student1
 #student2 = Student.from_s("last_name: Gradel, first_name: Oleg, patronymic: Olegоvich")
 puts student1.getInfo
+#puts hash = student1.getInfo.split(", ").map {|i| i.split(": ")}.to_h["fullname"]
+
+student2 = Student.new(last_name: "Gradel", first_name: "Blexander", patronymic: "Olegоvich", phone: "+79182522066", id: 2, telegram: "gradel1", email: "gradel1@gmail.com", git: "gradel1")
+puts student2.getInfo
