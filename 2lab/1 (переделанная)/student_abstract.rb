efe7c68e7bb61a9aadb001bd7553abc2f1e12919
gradel1 <96313800+gradel1@users.noreply.github.com>
2023-05-27 09:57:28 +0300

    Update student_abstract.rb

--- 2lab/1 (переделанная)/student_abstract.rb
+++ 2lab/1 (переделанная)/student_abstract.rb
@@ -1,15 +1,15 @@
 class Student_abstract
   attr_reader :id, :git
-	private_class_method :new
-  
-  def self.any_contact(hash: {})
-	if hash["phone"] != nil then return hash["phone"] end
-      	if hash["email"] != nil then return hash["email"] end
-      	if hash["telegram"] != nil then return hash["telegram"] end
-      	if self.phone != nil then return "phone: #{self.phone}" end
-    	if self.email != nil then return "email: #{self.email}" end
-    	if self.telegram != nil then return "telegram: #{self.telegram}" end
-      	return nil  
-	end
-         
+  #private_class_method :new
+
+  def self.any_contact(hash: {},phone: nil, email: nil, telegram: nil)
+    if hash["phone"] != nil then return hash["phone"] end
+    if hash["email"] != nil then return hash["email"] end
+    if hash["telegram"] != nil then return hash["telegram"] end
+    if phone != nil then return "phone: #{phone}" end
+    if email != nil then return "email: #{email}" end
+    if telegram != nil then return "telegram: #{telegram}" end
+    return nil
+  end
+
 end
