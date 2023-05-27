b1c0f66409e28c63f8c3a8a163930343f78e217c
gradel1 <96313800+gradel1@users.noreply.github.com>
2023-05-27 09:57:09 +0300

    Update Student_short.rb

--- 2lab/1 (переделанная)/Student_short.rb
+++ 2lab/1 (переделанная)/Student_short.rb
@@ -1,26 +1,28 @@
-load('student.rb')
-load('student_abstract.rb')
+#load('student.rb')
+#load('student_abstract.rb')
+require_relative 'student'
+require_relative 'student_abstract'
 
 class Student_short < Student_abstract
-	attr_reader :fullname, :contact
+  attr_reader :fullname, :contact
 
-	def initialize(fullname:, git: nil, contact: nil, id: nil)
-		@id = id
-		@fullname = fullname
-		@git = git
-		@contact = contact
-	end
+  def initialize(fullname:, git: nil, contact: nil, id: nil)
+    @id = id
+    @fullname = fullname
+    @git = git
+    @contact = contact
+  end
 
-	def self.from_student(student)
-		Student_short.new(id: student.id, fullname: "#{student.last_name} #{student.first_name[0]}. #{student.patronymic[0]}.", git: student.git, contact: student.first_contact)
-	end
+  def self.from_student(student)
+    Student_short.new(id: student.id, fullname: "#{student.last_name} #{student.first_name[0]}. #{student.patronymic[0]}.", git: student.git, contact: Student_abstract.any_contact(phone: student.phone, email: student.email, telegram: student.telegram))
+  end
 
-	def self.from_s(s,id)
-		hash = s.split(", ").map {|i| i.split(": ")}.to_h
-		if git_valid?(hash["git"])
-			Student_short.new(fullname: hash["fullname"], id: id, git: hash["git"], contact: Student_abstract.any_contact(hash))
-		end
-	end
+  def self.from_s(s,id)
+    hash = s.split(", ").map {|i| i.split(": ")}.to_h
+    if Student.git_valid?(hash["git"])
+      Student_short.new(fullname: hash["fullname"], id: id, git: hash["git"], contact: Student_abstract.any_contact(hash: hash))
+    end
+  end
 
 end
 
