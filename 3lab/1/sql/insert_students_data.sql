154246a9d28a05fb79e98cb0d40f6978d5c79d87
gradel1 <96313800+gradel1@users.noreply.github.com>
2023-07-01 04:48:46 +0300

    Create insert_students_data.sql

---
+++ 3lab/1/sql/insert_students_data.sql
@@ -0,0 +1,3 @@
+insert into students(last_name, first_name, patronymic, phone, telegram, email, git) values ('Popov', 'Osip', 'Vasilyevich', '79181111111', NULL, NULL, NULL);
+insert into students(last_name, first_name, patronymic, phone, telegram, email, git) values ('Dmitriev', 'Oleg', 'Romanovich', NULL, NULL, 'aaa@gmail.com', NULL);
+insert into students(last_name, first_name, patronymic, phone, telegram, email, git) values ('Orlov', 'Yelisey', 'Mikhailovich', NULL, 'orlov', NULL, '0rlov');
